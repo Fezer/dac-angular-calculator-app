9dd3b34dfd3e28343051b39c8eb297e3262753dd
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-06-26 14:49:05 -0300

    App calculator implementation v1.0

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './calculadora';
+import { CalculadoraService } from './services';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalculadoraComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
   ]
 })
 export class CalculadoraModule { }
